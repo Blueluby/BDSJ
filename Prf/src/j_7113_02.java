dbda131aa9415f64c7fbec9d878497b4cd40244b
Administrator <Administrator@424-4>
2017-11-13 18:41:58 +0900

    타입설명

---
+++ Prf/src/j_7113_02.java
@@ -0,0 +1,22 @@
+
+public class j_7113_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 10;
+		System.out.println("num : "+num);
+		
+		boolean b = false;
+		System.out.println("b : "+b);
+		
+		byte by = 5;
+		System.out.println("by:"+by);
+		
+		char ch1 = 'A';
+		System.out.println("ch : "+ch1);
+		
+		float fo = 2.54f;
+		System.out.println("fo : "+fo);
+	}
+}
