289a12135394c04e60e75d5dcc0e29b7b45ce94c
Administrator <Administrator@424-4>
2017-11-13 18:42:35 +0900

    d

--- Prf/src/j_7113_01.java
+++ Prf/src/j_7113_01.java
@@ -5,5 +5,12 @@ public class j_7113 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		int n = 5;
+		
+		System.out.println("num : "+n);
+		System.out.println(1+2);
+		System.out.println("≥¢è°≥¢");
+		
 	}
 }
