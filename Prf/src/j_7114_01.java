3de7b57043b2c21f9bb75873e0704a6f20725fcf
Administrator <Administrator@424-4>
2017-11-14 14:24:04 +0900

    2일

--- Prf/src/j_7114_01.java
+++ Prf/src/j_7114_01.java
@@ -4,6 +4,14 @@ public class j_7114_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.print(3+5);
+		
+		char ch1 = 'a';
+		System.out.println("ch1 : "+ch1);
+		
+		short sh1 = ch1;
+		System.out.println("sh1 : "+sh1);
+		
 	}
 
 }
