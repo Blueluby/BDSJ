d8c58483bf7acca8bb8f76543063a0fc6b2cc2b4
Administrator <Administrator@424-4>
2017-11-14 15:27:41 +0900

    아직도 자료형 하고 앉아있음

--- Prf/src/j_7114_02.java
+++ Prf/src/j_7114_02.java
@@ -9,5 +9,12 @@ public class j_7114_02 {
 		System.out.println("in/dn = "+ iNum/(int)dNum );
 		System.out.println("in/2 = "+ iNum/2);
 		
+		final int NUM_LAST = 10;
+		
+		//final이 붙은 상수는 수정이 아니됨.
+		//NUM_LAST = 20;
+		
+		System.out.print("NUM : " +NUM_LAST);
+		
 	}
 }
