2a29f72bfb7c6bdf9f201060360236c081a42f0c
Administrator <Administrator@424-4>
2017-11-14 16:00:44 +0900

    .

---
+++ Prf/src/j_7114_03.java
@@ -0,0 +1,18 @@
+public class j_7114_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 10;
+		int n2 = -10;
+		double dd = 3.14;
+		char ch = 'a';
+
+		System.out.println("dd : " + dd);
+		System.out.println("ch : " + ch);
+
+		System.out.printf("dd : %.3f\n",dd);
+		System.out.printf("n1 : %+d\n",n1);
+		
+		System.out.printf("ch : %c\n", ch);
+	}
+}
---
+++ Prf/src/j_7114_04.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.System;
+
+public class j_7114_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("ют╥б : ");
+		int n = sc.nextInt();
+		
+		System.out.println("n : "+n);
+		
+	}
+
+}
