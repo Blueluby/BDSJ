e253ea36e3b7da3c469d99dcdb5bf3b62a8796d6
Administrator <Administrator@424-4>
2017-11-15 16:40:52 +0900

    .

---
+++ Prf/src/week1/j_7115_06.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class j_7115_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=7;
+		int n2, n3;
+		
+		n2 = n1++;
+		n3 = n1--;
+		
+		System.out.println("n1:"+n1);
+		System.out.println("n2:"+n2);
+		System.out.println("n3:"+n3);
+		
+	}
+
+}
---
+++ Prf/src/week1/j_7115_07.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class j_7115_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 7, n2;
+		n2=(n1--)+5;
+		
+		System.out.println("n1 - "+n1);
+		System.out.println("n2 - "+n2);
+	}
+
+}
