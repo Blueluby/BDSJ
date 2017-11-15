3c3cb4772f0178449dd3d89bcdde2441d6f4ecca
Administrator <Administrator@424-4>
2017-11-15 15:40:48 +0900

    .

---
+++ Prf/src/week1/j_7115_03.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class j_7115_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=20, n2=20;
+		
+		boolean re1 = (n1==20 && n1==20);
+		boolean re2 = (n2<=22 || n2 >=30);
+		
+		System.out.println("re2:"+re1);
+		System.out.println("re2:"+re2);
+		
+		if(!(n1==n2))
+			System.out.println("n1 == n2 == x");
+		else
+			System.out.println("n1 == n2");
+	}
+
+}
