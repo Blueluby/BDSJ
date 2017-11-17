48240f035f9992daa226ec0effbef498cabee5df
Administrator <Administrator@424-4>
2017-11-17 16:29:25 +0900

    switch 1

---
+++ Prf/src/week1/j_7117_02.java
@@ -0,0 +1,23 @@
+package week1;
+
+public class j_7117_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+		
+		switch(n) {
+		case 1:
+			System.out.println("Simple Java");
+		case 2:
+			System.out.println("Funny Java");
+		case 3:
+			System.out.println("Fantastic Java");
+		default :
+			System.out.println("The best programming language");
+		}
+		
+		System.out.println("Do you like coffe");
+	}
+
+}
