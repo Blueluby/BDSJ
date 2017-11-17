c0fea0b920ec94d0e55719fba8d4cef3e73e0ca1
Administrator <Administrator@424-4>
2017-11-17 16:29:44 +0900

    if -> switch

---
+++ Prf/src/week1/j_7118_03.java
@@ -0,0 +1,27 @@
+package week1;
+
+public class j_7118_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 24;
+		
+		switch(n/10) {
+		case 0:
+			System.out.println("0이상 10 미만의 수");
+			break;
+		case 1:
+			System.out.println("10이상 20미만의 수");
+			break;
+		case 2:
+			System.out.println("20이상 30미만의 수");
+			break;
+			
+		default:
+			System.out.println("30이상의 수");
+			
+		}
+		
+	}
+}
