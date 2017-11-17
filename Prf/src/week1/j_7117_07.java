d0963f78af3247b53c8fad2769e45164e307df0c
Administrator <Administrator@424-4>
2017-11-17 17:40:30 +0900

    String의 글자위치에 따른 swtich로 주민번호 남녀판독

---
+++ Prf/src/week1/j_7117_07.java
@@ -0,0 +1,29 @@
+package week1;
+
+import java.util.*;
+
+public class j_7117_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char gender;
+		String regNo = "";
+		
+		System.out.print("주민번호? (001122-1111222) :");
+		
+		Scanner sc = new Scanner(System.in);
+		regNo = sc.nextLine();
+		
+		gender = regNo.charAt(7);
+		switch(gender) {
+		case '1': case '3':
+			System.out.println("남자");
+			break;
+		case '2': case '4':
+			System.out.println("여자");
+			break;
+		default:
+			System.out.println("잘못된거");
+		}
+	}
+}
