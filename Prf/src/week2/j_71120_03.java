0798b8b8c919649d54cdeb5a19d9e84e7a8a6685
Administrator <Administrator@424-4>
2017-11-20 15:40:25 +0900

    구구단 2단

---
+++ Prf/src/week2/j_71120_03.java
@@ -0,0 +1,15 @@
+package week2;
+
+public class j_71120_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 2, cnt = 1;
+		System.out.println("2´Ü");
+		while(cnt<10) {
+			System.out.println("2*"+cnt+"="+n*cnt);
+			cnt ++;
+		}
+	}
+
+}
