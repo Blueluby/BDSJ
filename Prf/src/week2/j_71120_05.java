7cefabe430eeb658aa1ba9b441cf31c6819c104e
Administrator <Administrator@424-4>
2017-11-20 17:18:05 +0900

    100이하 자연수 2의 배수&7의 배수 그리고 합

---
+++ Prf/src/week2/j_71120_05.java
@@ -0,0 +1,24 @@
+package week2;
+
+public class j_71120_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cnt=1;
+		int tmp=0;
+		int sum = 0;
+		String str = "";
+		while(cnt<101) {
+			if((cnt%2==0) && (cnt%7==0)) {
+				str = cnt + ", ";
+				sum += cnt;
+				if(cnt>90)
+					str = cnt+"";
+				
+				System.out.print(str);
+			}
+			cnt++;
+		}
+		System.out.println("\n"+sum);
+	}
+}
