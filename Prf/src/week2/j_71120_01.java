34d4356b2d7bbe78bebf23abc9c134b667378d3c
Administrator <Administrator@424-4>
2017-11-20 15:37:30 +0900

    첫날

--- Prf/src/week2/j_71120_01.java
+++ Prf/src/week2/j_71120_01.java
@@ -4,7 +4,20 @@ public class j_71120_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int n1 =50, n2 = 100;
+		int b,d;
+		
+		b = (n1>n2) ? n1 : n2;
+		System.out.println(b);
+		
+		//d = (n1>n2) ? n1-n2 : n2-n1;
+		
+		if(n1>n2)
+			d = n1-n2;
+		else 
+			d=n2-n1;
+		System.out.println(d);
+		
 	}
-
 }
