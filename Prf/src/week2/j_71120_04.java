ad2996bdfef87ab699fe4bb0a889b28c3771149b
Administrator <Administrator@424-4>
2017-11-20 16:57:07 +0900

    1-10 합

--- Prf/src/week2/j_71120_04.java
+++ Prf/src/week2/j_71120_04.java
@@ -6,15 +6,20 @@ public class j_71120_04 {
 		// TODO Auto-generated method stub
 		int n=1;
 		int a=0;
+		String s = "";
 		while(n<11) {
 			a+=n++;
 			//System.out.println(a+", "+n++);
-			if(n!=11)
+			if(n!=11) {
 				System.out.print(a+"+");
-			else if(n==11)
+				s += a+"+";
+			}else if(n==11) {
 				System.out.print(a);
-				
+				s+=a;
+				//s = s.substring(0, s.length()-1) + "=" + a;
+			}	
 		}
+		System.out.println("\n"+s);
 	}
 
 }
