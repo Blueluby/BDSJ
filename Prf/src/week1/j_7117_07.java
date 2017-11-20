316411f6897806fef32d9d25195ed2e03420cce3
Administrator <Administrator@424-4>
2017-11-20 18:30:11 +0900

    .

--- Prf/src/week1/j_7117_07.java
+++ Prf/src/week1/j_7117_07.java
@@ -6,13 +6,14 @@ public class j_7117_07 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char gender;
+		char gender=' ';
 		String regNo = "";
 		
 		System.out.print("주민번호? (001122-1111222) :");
 		
 		Scanner sc = new Scanner(System.in);
 		regNo = sc.nextLine();
+		
 		
 		gender = regNo.charAt(7);
 		switch(gender) {
@@ -25,5 +26,18 @@ public class j_7117_07 {
 		default:
 			System.out.println("잘못된거");
 		}
+		
+		String aa = regNo.charAt(7)+"";
+		switch(aa) {
+		case "1": case "3":
+			System.out.println("남자");
+			break;
+		case "2": case "4":
+			System.out.println("여자");
+			break;
+		default:
+			System.out.println("잘못된거");
+		}
+		
 	}
 }
