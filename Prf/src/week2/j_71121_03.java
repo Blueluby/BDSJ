303703c2a585310b562d100ea672bdf38912ad86
Administrator <Administrator@424-4>
2017-11-21 15:27:54 +0900

    gugu

--- Prf/src/week2/j_71121_03.java
+++ Prf/src/week2/j_71121_03.java
@@ -5,10 +5,17 @@ public class j_71121_03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i = 1; i <= 9; i++) {
-			System.out.print("["+i+"단]\t");
+		for (int i = 0; i < 10; i++) {
+			if(i==0)
+				System.out.print("[구구단]\t");
+			else
+				System.out.print("["+i+"단]\t");
+			
 			for (int k = 1; k <= 9; k++) {
-				System.out.print(i + "x" + k + "=" + i * k + "\t");
+				if(i==0)
+					System.out.print("["+k+"단]\t");
+				else
+					System.out.print(i + "x" + k + "=" + i * k + "\t");
 			}
 			System.out.println("");
 		}
