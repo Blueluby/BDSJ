78b0b1e6062cfa2bb74a2861156265f344a9bb6d
Administrator <Administrator@424-4>
2017-11-21 15:10:36 +0900

    for문

--- Prf/src/week2/j_71121_02.java
+++ Prf/src/week2/j_71121_02.java
@@ -6,7 +6,9 @@ public class j_71121_02 {
 		// TODO Auto-generated method stub
 		int n = 1;
 		for(n=1; n<5; n++)
-			System.out.println("I like java");
+			System.out.println("I like java" + n);
+		
+		
 		int i=0, j=0;
 		for(i=0, j=5; i<j; i++, j--)
 			System.out.println("Ãâ·Â");
