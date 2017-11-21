6ae175ea00dcbd7ee0ba010f9014fd0d697f4cbd
Administrator <Administrator@424-4>
2017-11-21 15:41:12 +0900

    5 7 최소공배수

---
+++ Prf/src/week2/j_71121_05.java
@@ -0,0 +1,23 @@
+package week2;
+
+public class j_71121_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		boolean search=false;
+		
+		while(n<100) {
+			if(n%5==0 && n%7==0) {
+				search = true;
+				break;
+			}
+			n++;
+		}
+		
+		if(search)
+			System.out.println("Á¤¼ö:"+n);
+		else
+			System.out.println("x");
+	}
+}
