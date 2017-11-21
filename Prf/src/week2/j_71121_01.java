efe3614925bf4c4278718eabc2f78824b2b8a0bc
Administrator <Administrator@424-4>
2017-11-21 14:44:03 +0900

    2일 첫(어제거 한번 확인)

---
+++ Prf/src/week2/j_71121_01.java
@@ -0,0 +1,14 @@
+package week2;
+
+public class j_71121_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		
+		do {
+			System.out.println("I like java "+n);
+			n++;
+		}while(n<3);
+	}
+}
