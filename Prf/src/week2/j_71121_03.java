0dd5d31725d68d9692e613cf4404b32393cc6dc8
Administrator <Administrator@424-4>
2017-11-21 15:20:35 +0900

    gugudan

---
+++ Prf/src/week2/j_71121_03.java
@@ -0,0 +1,17 @@
+package week2;
+
+public class j_71121_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.print("["+i+"´Ü]\t");
+			for (int k = 1; k <= 9; k++) {
+				System.out.print(i + "x" + k + "=" + i * k + "\t");
+			}
+			System.out.println("");
+		}
+	}
+
+}
