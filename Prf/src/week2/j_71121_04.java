1bbd5240530455eb970d5212e2a22e72ebb9f2fd
Administrator <Administrator@424-4>
2017-11-21 15:28:08 +0900

    팩토리얼

---
+++ Prf/src/week2/j_71121_04.java
@@ -0,0 +1,14 @@
+package week2;
+
+public class j_71121_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=1;
+		for(int i = 1; i<=5; i++) {
+			sum *= i;
+			System.out.println("i:"+i+"\ttotal : "+sum);
+		}
+	}
+
+}
