c5fd807468fe1f62f92da49b39182ef751e6e079
Administrator <Administrator@424-4>
2017-11-22 15:13:20 +0900

    중첩포문, 달팽이문

--- Prf/src/week2/j_71122_02.java
+++ Prf/src/week2/j_71122_02.java
@@ -13,6 +13,46 @@ public class j_71122_02 {
 			}
 			System.out.println("");
 		}
+		
+		vtest();
 	}
-
+	
+	public static void vtest() {
+		int [][]i = new int[5][5];
+		
+		for(int n=0; n<5; n++) {
+			for(int m=0; m<5; m++) {
+				i[n][m] = 0;
+			}
+		}
+		
+		int a;
+		int x=0;
+		int y = -1;
+		int d = 1;
+		int p;
+		int cnt=1;
+		int jmax=5;
+		while(0<=jmax) {
+			for(a=0; a<jmax;a++) {
+				y+=d;
+				i[x][y]=cnt;
+				cnt++;
+			}
+			jmax--;
+			for(a=0;a<jmax;a++) {
+				x+=d;
+				i[x][y]=cnt;
+				cnt++;
+			}
+			d=d*-1;
+		}
+		
+		for(int q=0; q<5; q++) {
+			for(int w=0; w<5; w++) {
+				System.out.print(i[q][w]+"\t");
+			}
+			System.out.println("");
+		}
+	}
 }
