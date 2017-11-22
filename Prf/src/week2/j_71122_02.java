fb5dcacdc706f3dca27c274fe54e21f5c5238abc
Administrator <Administrator@424-4>
2017-11-22 14:50:43 +0900

    2차 for

---
+++ Prf/src/week2/j_71122_02.java
@@ -0,0 +1,18 @@
+package week2;
+
+public class j_71122_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		
+		for(i=0; i<3; i++) {
+			System.out.println("바깥"+(i+1)+"차");
+			for(j=0; j<3; j++) {
+				System.out.println("안쪽"+(j+1)+"차");
+			}
+			System.out.println("");
+		}
+	}
+
+}
