136a6f9820843174dafd50f736c5bf02f8e21648
Administrator <Administrator@424-4>
2017-11-23 15:17:40 +0900

    조건문 구겨넣기 무엇?

--- Prf/src/week2/j_71123_02.java
+++ Prf/src/week2/j_71123_02.java
@@ -4,6 +4,10 @@ public class j_71123_02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int s = 0, i = 0;
 		
+		while((s+=++i)<=100) {
+			System.out.printf("%d - %d\n", i, s);
+		}
 	}
 }
