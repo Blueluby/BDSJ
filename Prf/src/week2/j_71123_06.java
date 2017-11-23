df4301178910e8efc451e637ad4cbf3674f950b3
Administrator <Administrator@424-4>
2017-11-23 16:43:31 +0900

    정렬

--- Prf/src/week2/j_71123_06.java
+++ Prf/src/week2/j_71123_06.java
@@ -5,14 +5,14 @@ public class j_71123_06 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Loop1:
-			for(int i=2; i<10; i++) {
-				for(int j=2; j<10; j++) {
-					if(j==5)
-						break Loop1;
-					
-					System.out.println(i+"x"+j+"="+i*j);
-				}
-				System.out.println();
+		for(int i=2; i<10; i++) {
+			for(int j=2; j<10; j++) {
+				if(j==5)
+					break Loop1;
+				
+				System.out.println(i+"x"+j+"="+i*j);
 			}
+			System.out.println();
+		}
 	}
 }
