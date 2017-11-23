3963482e10ee4bed679ccf4e5447b001ce80a013
Administrator <Administrator@424-4>
2017-11-23 16:43:19 +0900

    제곱근

---
+++ Prf/src/week2/j_71123_07.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class j_71123_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("math.sqrt(4) = " + Math.sqrt(4));
+	}
+}
