7694b5122ae12a7320fdf59cd40ff6936e9b1883
Administrator <Administrator@424-4>
2017-11-24 17:25:27 +0900

    메서드

---
+++ Prf/src/week2/j_71124_05.java
@@ -0,0 +1,15 @@
+package week2;
+
+public class j_71124_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hiEveryone(12);
+		hiEveryone(28);
+	}
+	
+	public static void hiEveryone(int a) {
+		System.out.println("좋은아침입니다\n제 나이는 "+a+"세 입니다");
+	}
+
+}
