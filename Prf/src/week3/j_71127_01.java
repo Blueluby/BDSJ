301a97d8897fe14c0e03fe2d77e0d37884a5d32f
Administrator <Administrator@424-4>
2017-11-27 15:38:16 +0900

    연속된 글자로 된 문자열에 중복 갯수. 이전에 씌인건 확인 못함.

--- Prf/src/week3/j_71127_01.java
+++ Prf/src/week3/j_71127_01.java
@@ -8,16 +8,37 @@ public class j_71127_01 {
 		// TODO Auto-generated method stub
 		System.out.print("입력=");
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
+		String str = sc.nextLine() + ' ';
 		String strChar = "";
 		int cnt=0;
 		
 		for(int i=0; i<str.length()-1; i++) {
 			if(str.charAt(i)==str.charAt(i+1))
 				cnt++;
-			else
+			else {
 				strChar = strChar + str.charAt(i) + cnt ;
+				cnt=1;
+			}
 		}
-		System.out.println(strChar);
+		System.out.println("결과="+strChar);
+		
+		
+		//입력 받을 녀석을 쌓는 것
+//		for(int a = 0; a<str.length()-1; a ++) {
+//			if(a==0) {
+//				cnt++;
+//				strChar += str.charAt(a) + cnt; 
+//			}
+//			int ck=0;
+//			while(ck<str.length()) {
+//				if(str.charAt(a) == str.charAt(ck)) {
+//					
+//				}
+//				
+//				ck++;
+//			}
+//		}
+//		System.out.println(strChar);
+		
 	}
 }
