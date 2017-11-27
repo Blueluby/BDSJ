5276fc7f9180397d0e67632558b765eb74545f84
Administrator <Administrator@424-4>
2017-11-27 15:38:37 +0900

    정사각형의 넓이를 뱉어내는 클래스를 사용.

--- Prf/src/week3/j_71127_02.java
+++ Prf/src/week3/j_71127_02.java
@@ -1,10 +1,25 @@
 package week3;
 
-public class j_71127_02 {
+import java.util.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//정사각형 클래스
+class Sqrt{
+	//한 변의 길이
+	int length;
+	
+	//사각형의 넓이 반환
+	int size() {
+		return length*length;
 	}
+}
 
+public class j_71127_02 {
+	public static void main(String[] args) {
+		//정사각형의 한변의 길이와 크기를 구하는 class
+		Sqrt Sq = new Sqrt();
+		Scanner s = new Scanner(System.in);
+		System.out.print("변 길이 입력 :");
+		Sq.length = s.nextInt();
+		System.out.println("정사각형의 넓이는 : "+Sq.size());
+	}
 }
