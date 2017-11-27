2b96311ce7c8063fa17f602b52d70c03b8783792
Administrator <Administrator@424-4>
2017-11-27 16:54:48 +0900

    .

---
+++ Prf/src/week3/j_71127_04.java
@@ -0,0 +1,18 @@
+package week3;
+
+class Test{
+	final int num = 5;
+	
+	public void show() {
+		System.out.println("num : "+num);
+	}
+}
+
+public class j_71127_04 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t1 = new Test();
+		t1.show();
+	}
+
+}
