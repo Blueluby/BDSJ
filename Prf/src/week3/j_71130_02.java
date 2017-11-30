1fbb1ee752affc34b489333a2e3e0af1c716fca4
SJJ <ksu499@daum.net>
2017-11-30 15:57:26 +0900

    .

---
+++ Prf/src/week3/j_71130_02.java
@@ -0,0 +1,18 @@
+package week3;
+
+public class j_71130_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hi";
+		String str1 = "Hi";
+		
+		System.out.println("str : "+str);
+		
+		if(str == str1)
+			System.out.println("같은문자");
+		else
+			System.out.println("다른문자");
+	}
+
+}
---
+++ Prf/src/week3/j_71130_03.java
@@ -0,0 +1,17 @@
+package week3;
+
+public class j_71130_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] strArr = new String[3];
+		strArr[0] = new String("JAVA");
+		strArr[1] = new String("Flex");
+		strArr[2] = new String("Ruby");
+		
+		for(int i = 0 ; i <strArr.length; i++) {
+			System.out.println(strArr[i]);
+		}
+	}
+
+}
