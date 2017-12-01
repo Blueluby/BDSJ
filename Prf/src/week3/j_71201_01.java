6e118bf95cec3184c0d4360f696a63d218334c54
SJJ <ksu499@daum.net>
2017-12-01 14:18:31 +0900

    1부터 10까지의 임의의 값 배열 저장, 출력

---
+++ Prf/src/week3/j_71201_01.java
@@ -0,0 +1,16 @@
+package week3;
+
+public class j_71201_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r = 0;
+		int[] ar = new int[10];
+		
+		for(int i=0; i<ar.length; i++) {
+			ar[i] = (int)(Math.random()*10)+1;
+			System.out.println(ar[i]);
+		}
+	}
+
+}
