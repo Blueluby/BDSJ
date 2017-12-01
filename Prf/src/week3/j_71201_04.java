bbeacf57c623a75203e741e4a1124410ef6f54a1
SJJ <ksu499@daum.net>
2017-12-01 16:34:24 +0900

    .

--- Prf/src/week3/j_71201_04.java
+++ Prf/src/week3/j_71201_04.java
@@ -15,10 +15,19 @@ public class j_71201_04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		aNumber[] arr = new aNumber[] {
-				new aNumber(),
-				new aNumber(),
-				new aNumber()
+								new aNumber(),
+								new aNumber(),
+								new aNumber()
 		};
+		
+//		arr[0].num = 5;
+//		arr[1].num = 10;
+//		arr[2].num = 15;
+		for(int i=0; i<arr.length; i++)
+			arr[i].num = 5*(i+1);
+		
+		for(int i=0; i<arr.length; i++)
+			arr[i].setNum(5*(i+1));
 		
 		for(aNumber e:arr)
 			e.num++;
