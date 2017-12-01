5c3d67e16cd3088bae1bba9652cc27a98fbd1cbe
SJJ <ksu499@daum.net>
2017-12-01 16:36:28 +0900

    dll.

---
+++ Prf/src/dll/t1.java
@@ -0,0 +1,25 @@
+package dll;
+
+public class t1 {
+	public String bracket(String s) {
+		String str = "";
+		str = "["+s+"] ";
+		return str;
+	}
+	public String bracket(int i) {
+		String str = "";
+		str = "["+i+"] ";
+		return str;
+	}
+	
+	public String bracketln(String s) {
+		String str = "";
+		str = "["+s+"] ";
+		return str;
+	}
+	public String bracketln(int i) {
+		String str = "";
+		str = "["+i+"]";
+		return str;
+	}
+}
