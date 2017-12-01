3e28723225629ac393ed2f614bc3b219d81b6d80
SJJ <ksu499@daum.net>
2017-12-01 16:34:38 +0900

    최대최소

---
+++ Prf/src/week3/j_71201_05.java
@@ -0,0 +1,32 @@
+package week3;
+
+public class j_71201_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] score= {79,88,91,33,100,55,95};
+		
+//		int max=0;
+//		int min=100;
+	
+		int max = score[0];
+		int min = score[0];
+
+		for (int m = 0; m < score.length; m++) {
+			if (max < score[m]) {
+				System.out.print("최대값 갱신 : " + max + "->");
+				max = score[m];
+				System.out.println(score[m]);
+			} else if (min > score[m]) {
+				System.out.print("최소값 갱신 : " + min + "->");
+				min = score[m];
+				System.out.println(score[m]);
+			}
+		}
+		
+		System.out.println("\n최종\n");
+		System.out.println("최대:"+max);
+		System.out.println("최소:"+min);
+	}
+}
