9ec8d435af1d6afb0aa97dec1dc545987214cad7
SJJ <ksu499@daum.net>
2017-12-01 14:50:00 +0900

    중복값 검출

--- Prf/src/week3/j_71201_01.java
+++ Prf/src/week3/j_71201_01.java
@@ -1,16 +1,34 @@
 package week3;
 
-public class j_71201_01 {
+import dll.t1;
 
+public class j_71201_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		t1 stol = new t1();
+		
 		int r = 0;
 		int[] ar = new int[10];
 		
+		//배열에 1~10 랜덤값 입력 및 1차 출력
 		for(int i=0; i<ar.length; i++) {
 			ar[i] = (int)(Math.random()*10)+1;
-			System.out.println(ar[i]);
+			System.out.print(stol.bracket(ar[i]));
 		}
+		System.out.println();
+		
+		//검출
+		int [] cnt = new int[ar.length];
+		for(int i=0; i<ar.length; i++) {
+			for(int j=1; j<11; j++) {
+				if(ar[i] == j) {
+					cnt[(j-1)] +=1;
+				}
+			}
+		}
+		
+		//출력
+		for(int i=0; i<cnt.length; i++)
+			System.out.println((i+1) + "의 갯수 : "+cnt[i]);
 	}
-
 }
