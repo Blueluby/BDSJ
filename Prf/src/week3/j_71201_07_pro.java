b945f37754191fb63d94e398f42a274a0cd7c249
Administrator <Administrator@424-4>
2017-12-01 19:01:13 +0900

    문제

---
+++ Prf/src/week3/j_71201_07_pro.java
@@ -0,0 +1,10 @@
+package week3;
+
+public class j_71201_07_pro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1~9 사이의 중복되지 않는 숫자로 이루어진 3자리의 숫자를 만드시오
+	}
+
+}
