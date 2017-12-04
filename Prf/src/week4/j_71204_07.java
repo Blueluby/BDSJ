14e9fcc421cfc156f163bb8fc2584898bf26b2e6
SJJ <ksu499@daum.net>
2017-12-04 17:24:32 +0900

    문자배열 = 문자열 / 문자열 배열

---
+++ Prf/src/week4/j_71204_07.java
@@ -0,0 +1,22 @@
+package week4;
+
+public class j_71204_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names = {"Kim", "Park", "Yi"};
+		
+		for(int i=0; i<names.length; i++) {
+			System.out.println("names["+i+"]:"+names[i]);
+		}
+		
+		String tmp = names[2];
+		System.out.println("tmp:"+tmp);
+		
+		names[0]="Yu";
+		
+		for(String str:names)
+			System.out.println(str);
+	}
+
+}
