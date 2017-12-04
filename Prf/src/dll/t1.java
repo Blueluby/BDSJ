c698cca7fb5d87ed6b75c358f93bde0af8a4a957
SJJ <ksu499@daum.net>
2017-12-04 15:41:55 +0900

    dll update

--- Prf/src/dll/t1.java
+++ Prf/src/dll/t1.java
@@ -1,6 +1,13 @@
 package dll;
 
 public class t1 {
+	
+	public long startTime;
+	public long endTime;
+	
+	public t1() {
+		startTime = System.currentTimeMillis();
+	}
 	public String bracket(String s) {
 		String str = "";
 		str = "["+s+"] ";
@@ -21,5 +28,14 @@ public class t1 {
 		String str = "";
 		str = "["+i+"]";
 		return str;
+	}
+	
+	public void endTimeCk() {
+		endTime = System.currentTimeMillis();
+	}
+	
+	public void showCktime() {
+		long lTime = endTime - startTime;
+		System.out.println("TIME : " + lTime + "(ms)");
 	}
 }
