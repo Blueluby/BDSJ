ba6c9c45c5ae2edae68a6dcd7e09a7eead0d00ca
SJJ <ksu499@daum.net>
2017-12-05 16:33:48 +0900

    args 확인

---
+++ Prf/src/week4/j_71205_04.java
@@ -0,0 +1,15 @@
+package week4;
+
+import java.util.Arrays;
+
+public class j_71205_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0; i<args.length; i++) {
+			System.out.println("인덱스 문자열 "+i+"  "+args[i]);
+		}
+		
+	}
+}
