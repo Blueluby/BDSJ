ce978587d4e2165587f7956b7b9daa512a50c780
SJJ <ksu499@daum.net>
2017-12-06 11:22:17 +0900

    2차배열개념

---
+++ Prf/src/week4/j_71206_03.java
@@ -0,0 +1,28 @@
+package week4;
+
+public class j_71206_03 {
+	public static void main(String[] args) {
+		int[][] score = {
+							{100, 100, 100},
+							{20,20,30},
+							{30,30,30},
+							{40,40,40}
+		};
+		
+		int sum = 0;
+		
+		for(int i=0; i< score.length;i++) {
+			for(int j=0; j<score[i].length; j++) {
+				System.out.println("score["+i+"]["+j+"] = "+score[i][j]);
+			}
+		}
+		
+		for(int[] tmp:score) {
+			for(int i:tmp) {
+				sum+= i;
+			}
+		}
+		
+		System.out.println(sum);
+	}
+}
