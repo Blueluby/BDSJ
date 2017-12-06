b9a165f3416c842690ea585555cd62758b65330e
SJJ <ksu499@daum.net>
2017-12-06 12:20:29 +0900

    랜덤 퀴즈

---
+++ Prf/src/week4/j_71206_05.java
@@ -0,0 +1,31 @@
+package week4;
+
+import java.util.Scanner;
+
+public class j_71206_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[][] words= {
+				{"char","의자"},
+				{"computer","컴퓨터"},
+				{"integer","정수"}
+		};
+		
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<words.length; i++) {
+			int rand = ((int)Math.random()*3)+1;
+			System.out.println("Q"+i+". "+words[rand][0]+"의 뜻은?\t"+rand);
+			
+			String tmp = sc.nextLine();
+			
+			if(tmp.equals(words[rand][1])) {
+				System.out.println("정답");
+			}else {
+				System.out.println("틀렸. 답은 "+words[rand][1]);
+			}
+			
+		}
+	}
+
+}
