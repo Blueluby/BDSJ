81b8c4fc78e63e69f26648a05e2c7278a702229a
SJJ <ksu499@daum.net>
2017-12-11 11:59:28 +0900

    Object 상속 설명

---
+++ Prf/src/week5/j_71211_04.java
@@ -0,0 +1,19 @@
+package week5;
+
+public class j_71211_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Inherit in = new Inherit();
+		System.out.println("class 이름 : "+in.getClass());
+		System.out.println("in class를 표현하는 문자열 : "+in.toString());
+		System.out.println("in 출력 : "+in);
+		
+		
+	}
+
+}
+
+class Inherit{
+	
+}
