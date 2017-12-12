427a644c2c109b072581cce9b894da86ebeac7da
SJJ <ksu499@daum.net>
2017-12-12 15:42:41 +0900

    잠시 또 클래스 테스트

---
+++ Prf/src/week5/j_71212_05.java
@@ -0,0 +1,25 @@
+package week5;
+
+public class j_71212_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AAA a= new AAA();
+		BBB b = new BBB();
+		a.show();
+		b.show();
+	}
+
+}
+
+class AAA{
+	void show() {
+		System.out.println("AAA class");
+	}
+}
+
+class BBB{
+	void show() {
+		System.out.println("BBB class");
+	}
+}
