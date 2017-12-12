387c2e02f1836ad1e0d37822f7f3da8f7fedb6b3
SJJ <ksu499@daum.net>
2017-12-12 11:25:28 +0900

    .

--- Prf/src/week5/j_71212_01.java
+++ Prf/src/week5/j_71212_01.java
@@ -1,6 +1,7 @@
 package week5;
 
 import java.*;
+import java.util.Arrays;
 
 public class j_71212_01 {
 
@@ -14,13 +15,17 @@ public class j_71212_01 {
 		st.push(55);
 		st.push(13);
 		
+		st.showStack();
+		
 		st.pop();
 		st.pop();
+		st.showStack();
 		st.pop();
 		st.pop();
 		st.pop();
 		st.pop();
 		
+		st.showStack();
 	}
 	static int ran() {
 		int n = ((int)Math.random() * 9) + 1;
@@ -29,25 +34,31 @@ public class j_71212_01 {
 }
 
 class ArrayStack{
-	int i[];
-	int stack=0;
+	private int i[];
+	private int stack=0;
 	ArrayStack(int n){
 		i = new int[n];
 	}
 	public void push(int n) {
 		if(stack<i.length) {
-			i[stack] = n;
 			System.out.println("Inserted Item : "+n);
-			stack++;
+			i[stack++] = n;
+		}else if(stack==i.length) {
+			System.out.println("Inserting fail! Array Stack is full!");
 		}
 	}
 	public void pop() {
 		if(stack>0) {
-			System.out.println("Pop Item : "+i[stack-1]);
+			System.out.println("Pop : "+i[stack-1]);
 			i[stack-1] = 0;
 			stack--;
-		} else {
-			System.out.println("Over Range Error!");
+		} else if(stack<=0) {
+			System.out.println("Deleting fail! Array Stack is empty");
+			System.out.println("Pop : "+ (stack-1));
 		}
+	}
+	
+	public void showStack() {
+		System.out.println(Arrays.toString(i));
 	}
 }
