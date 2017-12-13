d94126bbfc5c57aa412438dbc057252696cad7fa
SJJ <ksu499@daum.net>
2017-12-13 14:45:38 +0900

    get/set

---
+++ Prf/src/week5/j_71213_03.java
@@ -0,0 +1,30 @@
+package week5;
+
+public class j_71213_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		_Point p = new _Point();
+		p.setX(105);;
+		
+		p.show();
+	}
+
+}
+
+class _Point{
+	private int x;
+	private int y;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		if(x<100 && x>0)
+			this.x = x;
+	}
+
+	void show() {
+		System.out.println("x : "+x+", y : "+y);
+	}
+}
