591d95cb1ff08f6fa019ec027ea6160ad00057a2
SJJ <ksu499@daum.net>
2017-12-18 16:09:02 +0900

    inner 1

---
+++ Prf/src/week6/j_71218_07.java
@@ -0,0 +1,52 @@
+package week6;
+
+import week6.Outer.Inner;
+
+public class j_71218_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Outer o = new Outer();
+		o.show();
+		
+		Outer.Inner i = o.new Inner();
+		
+		i.show_in();	
+		
+		
+	}
+}
+
+class Outer{
+	int x = 100;
+	private static int y = 200;
+	
+	public void show() {
+		System.out.println("[outer class start]");
+		System.out.println("x : "+x);
+		System.out.println("y : "+y);
+		System.out.println("[outer class end]");
+		
+		Inner cn = new Inner();
+		System.out.println("Inners member "+ cn.a);
+	}
+	
+	public Inner Inner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	class Inner{
+		int a = 1;
+		//private static int b = 2;
+		
+		public void show_in() {
+			System.out.println("[inner class start]");
+			System.out.println("x : "+x);
+			System.out.println("y : "+y);
+			System.out.println("a : "+a);
+			System.out.println("[inner class end]");
+		}
+		
+	}
+}
