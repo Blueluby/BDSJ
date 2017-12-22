ee9c9804a10751acb72dd0a83ba65d6d813a48f6
SJJ <ksu499@daum.net>
2017-12-22 16:38:36 +0900

    제네릭, 와일드카드

--- Prf/src/week6/j_71222_06.java
+++ Prf/src/week6/j_71222_06.java
@@ -2,8 +2,8 @@ package week6;
 
 public class j_71222_06 {
 
-	public static void openAndShowFruitBox(_FruitBox box) {
-		_Fruit f = (_Fruit)box.pullOut();
+	public static void openAndShowFruitBox(_FruitBox<? super _Apple> box) {		//제너릭 클래스 참조자료형. 자료형 제한하는 법. 형변환을 해주어야 함
+		_Fruit f = (_Fruit) box.pullOut();						// ? extends _Fruit		//물음표 기호는 자기자신을 포함한 모든 클래스 형변환 안해도 일단 됨.
 		f.showYou();
 	}
 	
@@ -18,6 +18,14 @@ public class j_71222_06 {
 		openAndShowFruitBox(box1);
 		openAndShowFruitBox(box2);
 		
+		_FruitBox<_Orange> box3 = new _FruitBox<_Orange>();
+		_FruitBox<_SubApple> box4 = new _FruitBox<_SubApple>();
+		
+//		openAndShowFruitBox(box3);
+//		openAndShowFruitBox(box4);
+		
+		
+		
 	}
 }
 
@@ -27,12 +35,17 @@ class _Fruit{
 	}
 }
 
+class _Orange{}
+
 class _Apple extends _Fruit{
 	public void showYou() {
 		super.showYou();
 		System.out.println("난 붉은 과일");
 	}
 }
+
+class _SubApple extends _Apple{}
+
 
 class _FruitBox<T>{
 	T item;
