f673a69dba89306344a765698bd19e1b0df2e3c4
SJJ <ksu499@daum.net>
2017-12-27 09:43:34 +0900

    리스트

---
+++ Prf/src/week7/j_71226_03.java
@@ -0,0 +1,19 @@
+package week7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class j_71226_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		list.add(11);
+		list.add(new Integer(22));
+		
+		System.out.println(Arrays.toString(list.toArray()));
+	}
+
+}
