a8c67c53cb972f6af07595c9aa4e341f66bd1fdd
SJJ <ksu499@daum.net>
2018-01-05 12:12:50 +0900

    입력방식

---
+++ Prf/src/week8/j_80104_02.java
@@ -0,0 +1,22 @@
+package week8;
+
+import java.util.Scanner;
+
+public class j_80104_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		System.out.println("숫자 : "+a);
+		sc.nextLine();
+		String str = sc.nextLine().trim();
+		System.out.println("문자열 : "+str);
+		
+		
+		
+	}
+
+}
